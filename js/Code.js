1b4703c34ef461a5136af5a4081c8a87bad8d798
dakshrandhawa <77522963+dakshrandhawa@users.noreply.github.com>
2021-06-02 20:48:52 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,26 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,75)
+
+    fill("white")
+    textSize(15)
+    text("C U T N I F O N", 700,150);
+    fill("lightblue")
+    text("Hint: Perform a particular task !!", 700,175)
+
+    fill("white")
+    textSize(15)
+    text("A T A D A S B E", 100,350)
+    fill("lightblue")
+    text("Hint: Store all information !!", 100,370)
+    
+
+}
